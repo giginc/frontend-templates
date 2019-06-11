1721e782b408578711ded83db965a4e246060e12
kokushin <mail@kokush.in>
2019-06-11 14:56:40 +0900

    add comment

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+// モジュールの読み込み
 const gulp = require('gulp');
 const del = require('del');
 const plumber = require('gulp-plumber');
@@ -12,19 +13,40 @@ const pngquant = require('imagemin-pngquant');
 const mozjpeg = require('imagemin-mozjpeg');
 const webpackConfig = require('./webpack.config');
 
+//
+// ----- 設定 -----
+//
+// * 必要であれば変更可能
+// * 変更を加えた際はREADME.mdに追記すること
+//
 const options = {
+
+  // コンパイル用に入力するディレクトリを指定
   SRC_PATH: './src',
-  PUBLIC_PATH: './public',
+
+  // ビルドしたファイルを出力するディレクトリを指定
   BUILD_PATH: './dist',
+
+  // ローカルサーバーで監視するディレクトリを指定
+  PUBLIC_PATH: './public',
+
+  // Autoprefixerの設定
+  // https://github.com/postcss/autoprefixer
   AUTOPREFIXER: {
     cascade: false,
   },
+
+  // BrowserSyncの設定
+  // https://www.browsersync.io/docs
   BROWSERSYNC: {
     server: './public',
     port: 3000,
     ghostMode: false,
     notify: false,
   },
+
+  // imageminの設定
+  // https://github.com/sindresorhus/gulp-imagemin
   IMAGEMIN: [
     pngquant('65-80'),
     mozjpeg({
@@ -35,26 +57,44 @@ const options = {
     imagemin.optipng(),
     imagemin.gifsicle(),
   ],
+
+  // ビルド時にJSを圧縮する
   MINIFY_JS: true,
+
+  // ビルド時にCSSを圧縮する
   MINIFY_CSS: true,
+
+  // ビルド時に画像を圧縮する
   MINIFY_IMG: true,
 };
 
+
+//
+// ----- 開発用タスク -----
+//
+// * $ npm run dev で実行
+// * ローカルサーバーの起動、オートコンパイル、ライブリロード
+//
+
+// HTMLファイルが変更された際にブラウザをリロードする
 gulp.task('html-reload', (done) => {
   browserSync.reload();
   done();
 });
 
+// ES6->ES5へのトランスパイル
 gulp.task('js', () => gulp.src(`${options.SRC_PATH}/js/entries/**/*.js`)
   .pipe(plumber())
   .pipe(webpack(Object.assign({}, webpackConfig, { mode: 'development' })))
   .pipe(gulp.dest(`${options.PUBLIC_PATH}/assets/js`)));
 
+// JSファイルが変更されたブラウザをリロードする
 gulp.task('js-reload', gulp.series('js'), (done) => {
   browserSync.reload();
   done();
 });
 
+// SCSS->CSSへのコンパイル
 gulp.task('scss', () => gulp.src(`${options.SRC_PATH}/scss/entries/**/*.scss`)
   .pipe(sassGlob())
   .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
@@ -62,6 +102,7 @@ gulp.task('scss', () => gulp.src(`${options.SRC_PATH}/scss/entries/**/*.scss`)
   .pipe(gulp.dest(`${options.PUBLIC_PATH}/assets/css`))
   .pipe(browserSync.stream()));
 
+// 各種アセットファイルの変更を監視する
 gulp.task('watch', () => {
   gulp.watch([`${options.SRC_PATH}/**/*.html`], gulp.task('html-reload'));
   gulp.watch([`${options.SRC_PATH}/js/**/*.js`], gulp.task('js-reload'));
@@ -69,32 +110,47 @@ gulp.task('watch', () => {
   browserSync.init(options.BROWSERSYNC);
 });
 
+// 初期化用
 gulp.task('default', gulp.series(
   'scss',
   'js',
   'watch',
 ));
 
+
+//
+// ----- ビルド用タスク -----
+//
+// * $ npm run build で実行
+// * アセットファイルの圧縮
+// * 基本的にこのコマンドで生成されたものを納品する
+//
+
+// 既存の`dist`ディレクトリを削除する
 gulp.task('clean', () => del([
   options.BUILD_PATH,
 ]));
 
+// `public`ディレクトリから`dist`ディレクトリへファイルをコピーする
 gulp.task('copy', () => gulp
   .src(`${options.PUBLIC_PATH}/**`, {
     base: options.PUBLIC_PATH,
   })
   .pipe(gulp.dest(options.BUILD_PATH)));
 
+// JSを圧縮する
 gulp.task('minify:js', () => options.MINIFY_JS && gulp.src(`${options.BUILD_PATH}/assets/js/**/*.js`)
   .pipe(plumber())
   .pipe(webpack(Object.assign({}, webpackConfig, { mode: 'production' })))
   .pipe(gulp.dest(`${options.BUILD_PATH}/assets/js`)));
 
+// CSSを圧縮する
 gulp.task('minify:css', () => options.MINIFY_CSS && gulp
   .src(`${options.BUILD_PATH}/assets/css/**/*.css`)
   .pipe(cssmin())
   .pipe(gulp.dest(`${options.BUILD_PATH}/assets/css`)));
 
+// 画像を圧縮する
 gulp.task('minify:img', () => options.MINIFY_IMG && gulp
   .src(`${options.BUILD_PATH}/assets/img/*`)
   .pipe(imagemin(options.IMAGEMIN, {
@@ -102,6 +158,7 @@ gulp.task('minify:img', () => options.MINIFY_IMG && gulp
   }))
   .pipe(gulp.dest(`${options.BUILD_PATH}/assets/img`)));
 
+// 初期化用
 gulp.task('build', gulp.series(
   'clean',
   'copy',
