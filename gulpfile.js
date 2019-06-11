51409b9232446ae2d90c3e1c2aa1a030d9ce2147
kokushin <mail@kokush.in>
2019-06-11 10:32:34 +0900

    jsの出力設定をgulpからwebpack.configに変更

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ const webpack = require('webpack-stream');
 const browserSync = require('browser-sync').create();
 const runSequence = require('run-sequence');
 const cssmin = require('gulp-cssmin');
+const webpackConfig = require('./webpack.config');
 
 const options = {
   SRC_PATH: './src',
@@ -18,32 +19,10 @@ const options = {
     cascade: false,
   },
   BROWSERSYNC: {
-    open: false,
     server: './public',
     port: 3000,
     ghostMode: false,
     notify: false,
-  },
-  WEBPACK: {
-    entry: {
-      common: './src/js/entries/common.js',
-      top: './src/js/entries/top.js',
-    },
-    output: {
-      filename: '[name].js',
-    },
-    resolve: {
-      extensions: ['.js', '.css'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: ['babel-loader'],
-          exclude: /node_modules/
-        },
-      ],
-    },
   },
   MINIFY_JS: true,
   MINIFY_CSS: true,
@@ -54,37 +33,27 @@ gulp.task('html-reload', (done) => {
   done();
 });
 
-gulp.task('js', () => {
-  return gulp.src(`${options.SRC_PATH}/js/entries/**/*.js`)
-    .pipe(plumber())
-    .pipe(webpack(Object.assign({}, options.WEBPACK, { mode: 'development' })))
-    .pipe(gulp.dest(`${options.PUBLIC_PATH}/assets/js`));
-});
+gulp.task('js', () => gulp.src(`${options.SRC_PATH}/js/entries/**/*.js`)
+  .pipe(plumber())
+  .pipe(webpack(Object.assign({}, webpackConfig, { mode: 'development' })))
+  .pipe(gulp.dest(`${options.PUBLIC_PATH}/assets/js`)));
 
 gulp.task('js-reload', ['js'], (done) => {
   browserSync.reload();
   done();
 });
 
-gulp.task('scss', () => {
-  return gulp.src(`${options.SRC_PATH}/scss/entries/**/*.scss`)
-    .pipe(sassGlob())
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(autoprefixer(options.AUTOPREFIXER))
-    .pipe(gulp.dest(`${options.PUBLIC_PATH}/assets/css`))
-    .pipe(browserSync.stream());
-});
+gulp.task('scss', () => gulp.src(`${options.SRC_PATH}/scss/entries/**/*.scss`)
+  .pipe(sassGlob())
+  .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+  .pipe(autoprefixer(options.AUTOPREFIXER))
+  .pipe(gulp.dest(`${options.PUBLIC_PATH}/assets/css`))
+  .pipe(browserSync.stream()));
 
 gulp.task('watch', () => {
-  watch([`${options.SRC_PATH}/**/*.html`], () => {
-    return gulp.start(['html-reload']);
-  });
-  watch([`${options.SRC_PATH}/js/**/*.js`], () => {
-    return gulp.start(['js-reload']);
-  });
-  watch([`${options.SRC_PATH}/scss/**/*.scss`], () => {
-    return gulp.start(['scss']);
-  });
+  watch([`${options.SRC_PATH}/**/*.html`], () => gulp.start(['html-reload']));
+  watch([`${options.SRC_PATH}/js/**/*.js`], () => gulp.start(['js-reload']));
+  watch([`${options.SRC_PATH}/scss/**/*.scss`], () => gulp.start(['scss']));
   browserSync.init(options.BROWSERSYNC);
 });
 
@@ -92,47 +61,35 @@ gulp.task('default', () => {
   runSequence(
     'scss',
     'js',
-    'watch'
+    'watch',
   );
 });
 
-gulp.task('clean', () => {
-  return del([
-    options.BUILD_PATH
-  ]);
-});
+gulp.task('clean', () => del([
+  options.BUILD_PATH,
+]));
 
-gulp.task('copy', () => {
-  return gulp
-    .src(`${options.PUBLIC_PATH}/**`, {
-      base: options.PUBLIC_PATH
-    })
-    .pipe(gulp.dest(options.BUILD_PATH));
-});
+gulp.task('copy', () => gulp
+  .src(`${options.PUBLIC_PATH}/**`, {
+    base: options.PUBLIC_PATH,
+  })
+  .pipe(gulp.dest(options.BUILD_PATH)));
 
-gulp.task('minify:js', () => {
-  if (options.MINIFY_JS) {
-    return gulp.src(`${options.BUILD_PATH}/assets/js/**/*.js`)
-      .pipe(plumber())
-      .pipe(webpack(Object.assign({}, options.WEBPACK, { mode: 'production' })))
-      .pipe(gulp.dest(`${options.BUILD_PATH}/assets/js`));
-  }
-});
+gulp.task('minify:js', () => options.MINIFY_JS && gulp.src(`${options.BUILD_PATH}/assets/js/**/*.js`)
+  .pipe(plumber())
+  .pipe(webpack(Object.assign({}, webpackConfig, { mode: 'production' })))
+  .pipe(gulp.dest(`${options.BUILD_PATH}/assets/js`)));
 
-gulp.task('minify:css', () => {
-  if (options.MINIFY_CSS) {
-    return gulp
-      .src(`${options.BUILD_PATH}/assets/css/**/*.css`)
-      .pipe(cssmin())
-      .pipe(gulp.dest(`${options.BUILD_PATH}/assets/css`));
-  }
-});
+gulp.task('minify:css', () => options.MINIFY_CSS && gulp
+  .src(`${options.BUILD_PATH}/assets/css/**/*.css`)
+  .pipe(cssmin())
+  .pipe(gulp.dest(`${options.BUILD_PATH}/assets/css`)));
 
 gulp.task('build', () => {
   runSequence(
     'clean',
     'copy',
     'minify:js',
-    'minify:css'
+    'minify:css',
   );
 });
