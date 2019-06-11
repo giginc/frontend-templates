7b277c73576fc4329bf77e7bc298f30c7a567e16
kokushin <mail@kokush.in>
2019-06-11 10:45:50 +0900

    option.webpackを削除

--- gulpfile.js
+++ gulpfile.js
@@ -25,27 +25,6 @@ const options = {
     ghostMode: false,
     notify: false,
   },
-  WEBPACK: {
-    entry: {
-      common: './src/js/entries/common.js',
-      top: './src/js/entries/top.js',
-    },
-    output: {
-      filename: '[name].js',
-    },
-    resolve: {
-      extensions: ['.js', '.css'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: ['babel-loader'],
-          exclude: /node_modules/,
-        },
-      ],
-    },
-  },
   PUG: {
     pretty: true,
   },
