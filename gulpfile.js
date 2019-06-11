83ef90f2ef28cb5c7e410591fb897d105377f41b
kokushin <mail@kokush.in>
2019-06-11 10:45:04 +0900

    jsの出力設定をgulpからwebpack.configに変更

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const webpack = require('webpack-stream');
 const browserSync = require('browser-sync').create();
 const runSequence = require('run-sequence');
 const cssmin = require('gulp-cssmin');
+const webpackConfig = require('./webpack.config');
 
 const options = {
   SRC_PATH: './src',
@@ -29,28 +30,6 @@ const options = {
   EJS: {
     config: '_config.json',
   },
-  WEBPACK: {
-    entry: {
-      common: './src/js/entries/common.js',
-      top: './src/js/entries/top.js',
-      sample: './src/js/entries/sample.js',
-    },
-    output: {
-      filename: '[name].js',
-    },
-    resolve: {
-      extensions: ['.js', '.css'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: ['babel-loader'],
-          exclude: /node_modules/
-        },
-      ],
-    },
-  },
   MINIFY_JS: true,
   MINIFY_CSS: true,
 };
@@ -58,25 +37,25 @@ const options = {
 gulp.task('ejs', () => {
   const configFile = `${options.SRC_PATH}/ejs/${options.EJS.config}`;
 
-  fs.access(configFile, fs.R_OK | fs.W_OK, function (err) {
+  fs.access(configFile, fs.R_OK || fs.W_OK, (err) => {
     const config = (err) ? {} : JSON.parse(fs.readFileSync(configFile, 'utf8'));
     return gulp.src(
       [
         `${options.SRC_PATH}/ejs/**/*.ejs`,
         `!${options.SRC_PATH}/ejs/**/_*.ejs`,
-      ]
+      ],
     )
       .pipe(plumber({
-        errorHandler: function (_err) {
+        errorHandler: (_err) => {
           log.error(_err);
           this.emit('end');
-        }
+        },
       }))
       .pipe(ejs({
-        config: config,
+        config,
       }, {}, {
-          ext: '.html'
-        }))
+        ext: '.html',
+      }))
       .pipe(gulp.dest(`${options.PUBLIC_PATH}`));
   });
 });
@@ -86,40 +65,28 @@ gulp.task('html-reload', ['ejs'], (done) => {
   done();
 });
 
-gulp.task('js', () => {
-  return gulp.src(`${options.SRC_PATH}/js/entries/**/*.js`)
-    .pipe(plumber())
-    .pipe(webpack(Object.assign({}, options.WEBPACK, { mode: 'development' })))
-    .pipe(gulp.dest(`${options.PUBLIC_PATH}/assets/js`));
-});
+gulp.task('js', () => gulp.src(`${options.SRC_PATH}/js/entries/**/*.js`)
+  .pipe(plumber())
+  .pipe(webpack(Object.assign({}, webpackConfig, { mode: 'development' })))
+  .pipe(gulp.dest(`${options.PUBLIC_PATH}/assets/js`)));
 
 gulp.task('js-reload', ['js'], (done) => {
   browserSync.reload();
   done();
 });
 
-gulp.task('scss', () => {
-  return gulp.src(`${options.SRC_PATH}/scss/entries/**/*.scss`)
-    .pipe(sassGlob())
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(autoprefixer(options.AUTOPREFIXER))
-    .pipe(gulp.dest(`${options.PUBLIC_PATH}/assets/css`))
-    .pipe(browserSync.stream());
-});
+gulp.task('scss', () => gulp.src(`${options.SRC_PATH}/scss/entries/**/*.scss`)
+  .pipe(sassGlob())
+  .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+  .pipe(autoprefixer(options.AUTOPREFIXER))
+  .pipe(gulp.dest(`${options.PUBLIC_PATH}/assets/css`))
+  .pipe(browserSync.stream()));
 
 gulp.task('watch', () => {
-  watch([`${options.SRC_PATH}/ejs/**/*.ejs`], () => {
-    return gulp.start(['html-reload']);
-  });
-  watch([`${options.SRC_PATH}/ejs/${options.EJS.config}`], () => {
-    return gulp.start(['html-reload']);
-  });
-  watch([`${options.SRC_PATH}/js/**/*.js`], () => {
-    return gulp.start(['js-reload']);
-  });
-  watch([`${options.SRC_PATH}/scss/**/*.scss`], () => {
-    return gulp.start(['scss']);
-  });
+  watch([`${options.SRC_PATH}/ejs/**/*.ejs`], () => gulp.start(['html-reload']));
+  watch([`${options.SRC_PATH}/ejs/${options.EJS.config}`], () => gulp.start(['html-reload']));
+  watch([`${options.SRC_PATH}/js/**/*.js`], () => gulp.start(['js-reload']));
+  watch([`${options.SRC_PATH}/scss/**/*.scss`], () => gulp.start(['scss']));
   browserSync.init(options.BROWSERSYNC);
 });
 
@@ -128,47 +95,35 @@ gulp.task('default', () => {
     'ejs',
     'scss',
     'js',
-    'watch'
+    'watch',
   );
 });
 
-gulp.task('clean', () => {
-  return del([
-    options.BUILD_PATH
-  ]);
-});
+gulp.task('clean', () => del([
+  options.BUILD_PATH,
+]));
 
-gulp.task('copy', () => {
-  return gulp
-    .src(`${options.PUBLIC_PATH}/**`, {
-      base: options.PUBLIC_PATH
-    })
-    .pipe(gulp.dest(options.BUILD_PATH));
-});
+gulp.task('copy', () => gulp
+  .src(`${options.PUBLIC_PATH}/**`, {
+    base: options.PUBLIC_PATH,
+  })
+  .pipe(gulp.dest(options.BUILD_PATH)));
 
-gulp.task('minify:js', () => {
-  if (options.MINIFY_JS) {
-    return gulp.src(`${options.BUILD_PATH}/assets/js/**/*.js`)
-      .pipe(plumber())
-      .pipe(webpack(Object.assign({}, options.WEBPACK, { mode: 'production' })))
-      .pipe(gulp.dest(`${options.BUILD_PATH}/assets/js`));
-  }
-});
+gulp.task('minify:js', () => options.MINIFY_JS && gulp.src(`${options.BUILD_PATH}/assets/js/**/*.js`)
+  .pipe(plumber())
+  .pipe(webpack(Object.assign({}, options.WEBPACK, { mode: 'production' })))
+  .pipe(gulp.dest(`${options.BUILD_PATH}/assets/js`)));
 
-gulp.task('minify:css', () => {
-  if (options.MINIFY_CSS) {
-    return gulp
-      .src(`${options.BUILD_PATH}/assets/css/**/*.css`)
-      .pipe(cssmin())
-      .pipe(gulp.dest(`${options.BUILD_PATH}/assets/css`));
-  }
-});
+gulp.task('minify:css', () => options.MINIFY_CSS && gulp
+  .src(`${options.BUILD_PATH}/assets/css/**/*.css`)
+  .pipe(cssmin())
+  .pipe(gulp.dest(`${options.BUILD_PATH}/assets/css`)));
 
 gulp.task('build', () => {
   runSequence(
     'clean',
     'copy',
     'minify:js',
-    'minify:css'
+    'minify:css',
   );
 });
