25b5d89b684c0d88123b9cdf81040094e508cd0c
agehara hiroshi <agehara_hiroshi@giginc.co.jp>
2019-08-22 15:20:24 +0900

    update webpack.configのjsファイルで、ワイルドカードを使えるようにした

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
+const path = require("path");
+const glob = require("glob");
+const entries = {};
+const srcDir = "./src/js/entries"
+
+glob.sync('**/*.js', {
+  ignore: '**/_*.js',
+  cwd: srcDir
+}).map((value) => {
+  entries[value] = path.resolve(srcDir, value);
+});
+
+
 module.exports = {
-  entry: {
-    common: './src/js/entries/common.js',
-    top: './src/js/entries/top.js',
-  },
+  entry: entries,
   output: {
-    filename: '[name].js',
+    filename: '[name]',
   },
   resolve: {
     extensions: ['.js', '.css'],
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: ['babel-loader'],
-        exclude: /node_modules/,
-      },
-    ],
+    rules: [{
+      test: /\.js$/,
+      loader: ['babel-loader'],
+      exclude: /node_modules/,
+    }, ],
   },
 };
